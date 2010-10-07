49c3d96af001f776f2c5911dc1b897bda82937f8
jeffreyhorner <jeffrey.horner@gmail.com>
2010-10-07 17:21:21 -0500

    Adding plot support

--- routes.R
+++ routes.R
@@ -31,9 +31,6 @@ router$get("/dataset/:dataset/", function(dataset) {
 		render_brew("error",list(dataset=dataset))
 })
 
-###
-# The real web services
-###
 
 # Missing/broken part of sinartra
 render_json <- function(object) {
@@ -70,8 +67,55 @@ render_json <- function(object) {
 	sinartra:::render(json, mime_type = "application/json")
 }
 
+###
+# The real web services
+###
+
 router$get('/dataset/:dataset.json', function(dataset){
-	if (any(ls('package:datasets') == dataset))
-		render_json(get(dataset))
+	if (!any(ls('package:datasets') == dataset))
+		render_brew("error",list(dataset=dataset))
+
+	render_json(get(dataset))
+})
+
+bad_plot <- function(dataset){
+    t <- tempfile('png')
+    png(t)
+    #par(mar=rep(0,4))
+    plot(rnorm(100),main=paste('Fail for',dataset,' but Check it!'),col=rainbow(100,alpha=runif(100,0,1)),pch='.',cex=c(2,3,4,5,10,50,100))
+    dev.off()
+	payday <- readBin(t,'raw',file.info(t)$size)
+	unlink(t)
+	payday
+}
+
+router$get('/dataset/:dataset.png', function(dataset){
+
+	if (!any(ls('package:datasets') == dataset))
+		render_brew("error",list(dataset=dataset))
+
+	t <- tempfile('png')
+	cat('tempfile is',t,'\n')
+	png(t)
+    suppressWarnings(
+		eval(
+			substitute(
+				example(dataset,package='datasets',ask=FALSE),
+				list(dataset=dataset)
+			)
+		)
+	)
+	dev.off()
+	payday <- try(readBin(t,'raw',file.info(t)$size))
+	unlink(t)
+	if (inherits(payday,'try-error') || length(payday) <=1 ){
+		payday <- bad_plot(dataset)
+	}
+	list(
+		payload = payday,
+		"content-type" = 'image/png',
+		"headers" = c(),
+		"status code" = 200
+	)
 })
 
