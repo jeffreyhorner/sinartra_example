e7f6db22afc45d3a91b7e7d7443225e520907b5b
jeffreyhorner <jeffrey.horner@gmail.com>
2010-10-07 20:18:40 -0500

    More code documentation

--- routes.R
+++ routes.R
@@ -4,26 +4,31 @@
 library(datasets)
 library(RJSONIO)
 
-
-
-
-
 ###########################################
 # Top level HTML pages
 ###########################################
 
 router$get("/", function(...) {
+	
+	# brews the file index.html in the /views dir
 	render_brew("index",list(...))
 })
 
 router$get("/*.html", function(splat,query) {
+
+	# brews any *.html in the /views dir
 	render_brew(splat,list(query=query))
 })
 
 router$get("/public/*", function(splat, ...) {
-static_file(file.path(path, "public", splat))
+
+	# Returns any file as-is from the /public dir
+	static_file(file.path(path, "public", splat))
 })
 
+# This is the RESTful part of the application. Each 
+# dataset found in the datasets package has its own
+# URL
 router$get("/dataset/:dataset/", function(dataset) {
 	if (any(ls('package:datasets') == dataset))
 		render_brew("dataset",list(dataset=dataset))
@@ -32,10 +37,11 @@ router$get("/dataset/:dataset/", function(dataset) {
 })
 
 
-# Missing/broken part of sinartra
+# Convenience function to render an R object to JSON
+# Supposed to be part of sinartra, but not yet.
 render_json <- function(object) {
 
-	# Some datasets are instances of a sub-class of data.frame
+	# Some objects are instances of a sub-class of data.frame
 	# and RJSONIO doesn't know what to do with them, so we just
 	# use trickery.
 	if (inherits(object,'data.frame',which=TRUE) > 0){
@@ -43,7 +49,7 @@ render_json <- function(object) {
 		class(object) <- 'data.frame'
 
 		# Even these may have ts objects as columns so lets 
-		# just punt for right now.
+		# just punt for right now and assign it an NA column.
 		for (i in names(object)){
 			if (inherits(object[[i]],'ts')){
 				object[[i]] <- NA
@@ -67,9 +73,9 @@ render_json <- function(object) {
 	sinartra:::render(json, mime_type = "application/json")
 }
 
-###
-# The real web services
-###
+###########################################
+# Web services
+###########################################
 
 router$get('/dataset/:dataset.json', function(dataset){
 	if (!any(ls('package:datasets') == dataset))
@@ -78,6 +84,7 @@ router$get('/dataset/:dataset.json', function(dataset){
 	render_json(get(dataset))
 })
 
+# Returned when no dataset's example renders a plot
 bad_plot <- function(dataset){
     t <- tempfile('png')
     png(t)
@@ -97,7 +104,17 @@ router$get('/dataset/:dataset.png', function(dataset){
 	t <- tempfile('png')
 	cat('tempfile is',t,'\n')
 	png(t)
-    suppressWarnings(
+
+	# This is a bit of magic. R has an example function
+	# which runs example code located at the end of a
+	# particular help topic. Fortunately, there's a help
+	# topic for all datasets exported from the 'datasets'
+	# package. Unfortunately, not all of them produce a plot,
+	# and they can be noisy.
+	#
+	# This is where you would place your own data and plot routines, FYI
+	#
+	suppressWarnings(
 		eval(
 			substitute(
 				example(dataset,package='datasets',ask=FALSE),
@@ -105,12 +122,18 @@ router$get('/dataset/:dataset.png', function(dataset){
 			)
 		)
 	)
+
 	dev.off()
 	payday <- try(readBin(t,'raw',file.info(t)$size))
 	unlink(t)
+
 	if (inherits(payday,'try-error') || length(payday) <=1 ){
 		payday <- bad_plot(dataset)
 	}
+
+	# This is the low level return value that the R help web server
+	# expects. A list with four named elements. The payload must contain
+	# the entire response, regardless of size.
 	list(
 		payload = payday,
 		"content-type" = 'image/png',
